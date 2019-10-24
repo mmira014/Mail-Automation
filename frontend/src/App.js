6bf90fc34799f38aa7edbabd2ace2161b4765379
Marcos Miranda <mmira014@ucr.edu>
2019-10-23 17:59:31 -0700

    added basic homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
+  /*
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +23,72 @@ function App() {
       </header>
     </div>
   );
+  */
+
+  return (
+    <body>
+      <div class="sidenav">
+        <a href="#">City</a>
+        <a href="#">State</a>
+        <a href="#">Zipcode</a>
+        <a href="#">Upload</a>
+        <a href="#"></a>
+      </div>
+
+      <div className="main">
+        <header className="header">
+          <b>HOMEPAGE</b>
+        </header>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">Name</th>
+              <th scope="col">Street</th>
+              <th scope="col">City</th>
+              <th scope="col">State</th>
+              <th scope="col">Zipcode</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>{getName(user)}</td>
+              <td>{user.street}</td>
+              <td>{user.city}</td>
+              <td>{user.state}</td>
+              <td>{user.zipcode}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </body>
+  );
 }
+
+const user = {
+  id: '1',
+  name: 'john smith',
+  street: '1234 street st',
+  city: 'john\'s city',
+  state: 'john\'s state',
+  zipcode: '12345'
+}
+
+function getName(user) {
+  return user.name;
+}
+
+// function getAddrList(addrList) {
+//   for(var i = 0; i < addrList.size();++i) {
+//     <th>{addrList[i].id}</th>
+//     <th>{addrList[i].name}</th>
+//     <th>{addrList[i].street}</th>
+//     <th>{addrList[i].city}</th>
+//     <th>{addrList[i].state}</th>
+//     <th>{addrList[i].zipcode}</th>
+//   }
+// }
+
 
 export default App;
