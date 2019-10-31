2a53c3d6d8634c4756f611a7bd1e84ac319131a4
Marcos Miranda <mmira014@ucr.edu>
2019-10-30 17:54:51 -0700

    updated homepage to display array of users and styling stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,69 +3,7 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-function App() {
-  /*
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-  */
-
-  return (
-    <body>
-      <div class="sidenav">
-        <a href="#">City</a>
-        <a href="#">State</a>
-        <a href="#">Zipcode</a>
-        <a href="#">Upload</a>
-        <a href="#"></a>
-      </div>
-
-      <div className="main">
-        <header className="header">
-          <b>HOMEPAGE</b>
-        </header>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Name</th>
-              <th scope="col">Street</th>
-              <th scope="col">City</th>
-              <th scope="col">State</th>
-              <th scope="col">Zipcode</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>{getName(user)}</td>
-              <td>{user.street}</td>
-              <td>{user.city}</td>
-              <td>{user.state}</td>
-              <td>{user.zipcode}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </body>
-  );
-}
-
+var userArr = [];
 const user = {
   id: '1',
   name: 'john smith',
@@ -75,20 +13,111 @@ const user = {
   zipcode: '12345'
 }
 
-function getName(user) {
-  return user.name;
+// ------------ Classes ----------------------------------
+
+class App extends React.Component {
+  render() {
+    return (
+      <div >
+        <FilterOptions/>
+        <div className="main">
+          <header class="App-header">
+            <b>A Million Thanks: Addresses</b>
+          </header>
+          <table class="table">
+            <thead >
+              <tr class="tableHeader">
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Street</th>
+                <th scope="col">City</th>
+                <th scope="col">State</th>
+                <th scope="col">Zipcode</th>
+              </tr>
+            </thead>
+            <TableContent userArray={userArr}/>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-// function getAddrList(addrList) {
-//   for(var i = 0; i < addrList.size();++i) {
-//     <th>{addrList[i].id}</th>
-//     <th>{addrList[i].name}</th>
-//     <th>{addrList[i].street}</th>
-//     <th>{addrList[i].city}</th>
-//     <th>{addrList[i].state}</th>
-//     <th>{addrList[i].zipcode}</th>
-//   }
-// }
+class TableContent extends React.Component {
+  constructor(props) {
+    super(props);
+    getAddrList(this.props.userArray);
+  }
 
+  render() {
+    return (
+      <tbody>
+        {this.props.userArray.map((addr, index) => {
+          return (
+            <tr class="table">
+              <th>{addr.id}</th>
+              <td>{addr.name}</td>
+              <td>{addr.street}</td>
+              <td>{addr.city}</td>
+              <td>{addr.state}</td>
+              <td>{addr.zipcode}</td>
+            </tr>
+          )})
+        }
+      </tbody>
+    );
+  }
+}
+
+class FilterOptions extends React.Component {
+  render() {
+    return (
+      <div class="sidenav">
+        <div class="accordion"><h2><u>Filter:</u></h2></div>
+        <div class="panel"><p>test</p></div>
+        <button class="accordion"><h3>City</h3></button>
+        <div class="panel">sample text sample text sample text sample text sample text sample text sample text sample text sample text sample text </div>
+        <button class="accordion"><h3>State</h3></button>
+        <div class="panel">sample text</div>
+        <button class="accordion"><h3>Zipcode</h3></button>
+        <div class="panel">sample text</div>
+        <a href="#" class="uploadButton"><h1>Upload</h1></a>
+      </div>
+    );
+  }
+}
+
+// ------------- Functions -------------------------
+
+function getAddrList(addrList) {
+  for (var i = 0; i < 100; ++i) {
+    var newUser = {
+      id: i+1,
+      name: 'john'+(i.toString()), 
+      street: i.toString()+(' street st'),
+      city: 'john'+(i.toString())+('\'s city'),
+      state: 'john'+(i.toString())+('\'s city'),
+      zipcode: '12345'
+    };
+    userArr.push(newUser);
+  }
+}
+
+// --------- filter accordion behavior ----------------
+var accordion = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < accordion.length; ++i) {
+  accordion[i].addEventListener("click", function(){
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    }
+    else {
+      panel.style.display = "block";
+    }
+  })
+}
 
 export default App;
