824fd1dbb07e3148788119a5216db6435b7b0656
Marcos Miranda <mmira014@ucr.edu>
2019-11-20 15:03:24 -0800

    basic file upload functionality

--- frontend/src/Pages/Upload.js
+++ frontend/src/Pages/Upload.js
@@ -1,11 +1,40 @@
 import React from 'react'
 
 class UploadPage extends React.Component {
-    render() {
-      return (
-        <div><h1>Insert file upload box here</h1></div>
-      );
+  state = {
+    userFiles: null
+  }
+
+  // grabs selected files and stores array in state userFiles
+  fileSelectHandler = event => {
+    this.setState({
+      userFiles: event.target.files
+    }, () => this.printFiles(this.state.userFiles))
+  }
+
+  printFiles = files => {
+    for(var i = 0; i < files.length; ++i) {
+      console.log("File:", files[i])
     }
+    // console.log(this.state.userFiles[0])
+  }
+
+  // uploadFiles = _ => {
+  //   const files = this.state.userFiles;
+  //   fetch(`http://localhost:9000/upload`)
+  //   .then(response => response.json())
+  //   .then(this.getFiles)
+  //   .catch(err => console.err(err))
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>Insert file upload box here</h1>
+        <input type="file" multiple={true} onChange={this.fileSelectHandler} />
+      </div>
+    );
+  }
 }
 
 export default UploadPage;
