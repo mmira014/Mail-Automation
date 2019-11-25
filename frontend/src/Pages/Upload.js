a5e357af6e68db84752c9435e622e465963aa828
Marcos Miranda <mmira014@ucr.edu>
2019-11-24 19:26:11 -0800

    implemented image file upload to folder on server machine

--- frontend/src/Pages/Upload.js
+++ frontend/src/Pages/Upload.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import axios from 'axios'
+
+// function logFileBin(event) {
+//   console.log("logFileBin Output:\n",event.target.result);
+//   return event.target.result
+// }
 
 class UploadPage extends React.Component {
   state = {
@@ -7,9 +13,13 @@ class UploadPage extends React.Component {
 
   // grabs selected files and stores array in state userFiles
   fileSelectHandler = event => {
-    this.setState({
-      userFiles: event.target.files
-    }, () => this.printFiles(this.state.userFiles))
+    
+    // save files into userFiles[] and call upload function
+    this.setState(
+      {
+        userFiles: event.target.files
+      }, 
+      () => this.uploadFiles())
   }
 
   printFiles = files => {
@@ -19,19 +29,57 @@ class UploadPage extends React.Component {
     // console.log(this.state.userFiles[0])
   }
 
-  // uploadFiles = _ => {
-  //   const files = this.state.userFiles;
-  //   fetch(`http://localhost:9000/upload`)
-  //   .then(response => response.json())
-  //   .then(this.getFiles)
-  //   .catch(err => console.err(err))
-  // }
+  // transfer user uploaded files into ./tempresources folder
+  uploadFiles = async () => {
+    // var reader = new FileReader();
+    // var reader2 = new FileReader();
+
+    // reader.addEventListener('load', logFileBin); // will call logFileBin when load complete
+    // reader.addEventListener('load', logFileBin);
+
+    // reader.readAsBinaryString(this.state.userFiles[0])
+    // var strData = reader.readAsText(this.state.userFiles[0]);
+
+    console.log("First File:", this.state.userFiles[0])
+    // let fd = new FormData();
+    let files = this.state.userFiles;
+    let axiosArr = [];
+
+    console.log("File:", files[0], "\nfiles[0].name", files[0].name)
+    console.log("Num Files: ", files.length)
+    
+    for (var i = 0; i < files.length; ++i) {
+      let fd = new FormData();
+      fd.append('file', files[i], files[i].name) // FIXME: make sure set replaces on name
+      
+      const newPromise = axios({
+        method: 'post',
+        url: 'http://localhost:9000/upload', 
+        data: fd,
+      });
+      axiosArr.push(newPromise);
+    }
+
+    axios.all(axiosArr)
+    .catch((err) => {
+      console.log("Error:", err);
+    });
+    // reader.onload = function() {
+    //   let out = reader.result
+    //   console.log("out:", out)
+    //   fd.append('fileBinary', out)
+    //   axios.post('http://localhost:9000/upload',fd)
+    //   .then(response => console.log("Response:\n",response.data))
+    //   .catch(err => console.error(err))
+    // }
+    
+  }
 
   render() {
     return (
       <div>
-        <h1>Insert file upload box here</h1>
-        <input type="file" multiple={true} onChange={this.fileSelectHandler} />
+        {/* <h1>Insert file upload box here</h1> */}
+        <input type="file" multiple={true} accept="image/*" onChange={this.fileSelectHandler} />
       </div>
     );
   }
