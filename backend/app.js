0f84e4bed0ba2202a59dc228750c61c9224ad8be
Marcos Miranda <mmira014@ucr.edu>
2020-01-20 16:24:41 -0800

    Add credentials for local database

--- backend/app.js
+++ backend/app.js
@@ -20,9 +20,10 @@ var updateRouter = require('./routes/update');
 var app = express();
 
 var connection = mysql.createConnection({
-	host: '',
-	user: '',
-	database: ''
+	host: '127.0.0.1',
+	user: 'Mil',
+	password: '123MillionThanks!',
+	database: 'million_thanks'
 })
 
 connection.connect(function(err){
@@ -31,76 +32,11 @@ connection.connect(function(err){
 	}
 	else{
 		console.log("Error connecting database: " + err.message);
+		process.exit(1);
 	}
 })
 global.connection = connection;
-/*
-//THIS SECTION IS FOR DEMO PURPOSES ONLY (BEGIN)
-	var returnnames = [];
-	var returnstreets = [];
-	var returncities = [];
-	var returnstates = [];
-	var returnzip = [];
-	var returncapdate = [];
 
-
-	
-	var capturedate = new Date().toISOString().slice(0, 19).replace('T', ' ');
-	var index;
-	fs.readdir("tempresources", function(err, items){
-		
-		quickstart().then(addresses => {
-			console.log(addresses)
-			for (index = 0; index < addresses.length; index++){
-				//console.log(addresses[index][0].lat);
-				//console.log(addresses[index][0].lng);
-				//var testfile = fs.readFileSync("tempresources/" + items[index]);
-				var testfile = "tempresources/" + items[index];
-				var street_address = addresses[index][0].house_number + " " + addresses[index][0].road;
-				var insertquery = "INSERT INTO Postal_Address (Name, Street, City, State, Zip, Valid, File, Capture_date, Lat, Lng) VALUES (" + connection.escape("test") + "," + connection.escape(street_address ) + "," + connection.escape(addresses[index][0].city) + "," + connection.escape(addresses[index][0].state) + "," + connection.escape(addresses[index][0].zipcode) + "," + connection.escape(addresses[index][1]) + "," + connection.escape(testfile) + "," + connection.escape(capturedate) + "," + connection.escape(addresses[index][0].lat) + "," + connection.escape(addresses[index][0].lng) + ")";
-				connection.query(insertquery, function(err, result){
-					if (err){
-						//console.error('sql error: ', err);
-						//console.log('hi');
-						throw err
-					}
-					else{
-						console.log("sucessfully inserted");
-					}
-				})
-			}
-			
-			var retrievequery = "SELECT Name, Street, City, State, Zip, Capture_date FROM Postal_Address WHERE Valid = 'valid'";
-			connection.query(retrievequery, function(err, result){
-				if (err){
-					console.error('sql error: ', err);
-				}
-				else{
-					var i;
-						for (i = 0; i < result.length; i++){
-							returnnames.push(result[i].Name);
-							returnstreets.push(result[i].Street);
-							returncities.push(result[i].City);
-							returnstates.push(result[i].State);
-							returnzip.push(result[i].Zip);
-							returncapdate.push(result[i].Capture_date);
-						}
-						console.log(returnnames);
-						console.log(returnstreets);
-						console.log(returncities);
-						console.log(returnstates);
-						console.log(returnzip);
-						console.log(returncapdate);
-				}
-			});
-			
-		})
-		
-	})
-
-
-//THIS SECTION IS FOR DEMO PURPOSES ONLY (END)
-*/
 app.use(function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
